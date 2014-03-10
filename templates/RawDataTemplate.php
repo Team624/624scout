fc028e85daa037b671df6aef51213d990077322f
David Gros <dgros@earthlink.net>
2014-03-10 00:06:12 -0500

    fix raw data

--- templates/RawDataTemplate.php
+++ templates/RawDataTemplate.php
@@ -8,7 +8,6 @@ class RawDataTemplate extends Template {
   }
   public function render() {
   $matchData = $this->data['match_data'];
-  $cycles = $this->data['cycles'];
   ?>
    Match Data
    <?php if(isset($matchData[0])) { ?>
@@ -29,25 +28,7 @@ class RawDataTemplate extends Template {
     <?php } ?>
     </table>
   <?php } ?>
-  Cycles
-  <?php if(isset($cycles[0])) { ?>
-     <table id="cycles" class="raw-table">
-    <tr>
-      <?php foreach($cycles[0] as $key => $val) { 
-         if($val === NULL) $val = '--';
-      ?>
-        <th><p><?= $key ?></p></th>
-      <?php } ?>
-    </tr>
-    <?php foreach($cycles as $row) { ?>
-      <tr>
-        <?php foreach($row as $key => $val) { ?>
-          <td><div><?= $val ?></div></td>
-        <?php } ?>
-      </tr>
-    <?php } ?>
-    </table>
-  <?php } ?>
-  </div>
+  
+
   <?php }
 }
