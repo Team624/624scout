d9b3dfc7e02adfa1c06d6020bdc6a48663727e28
David Gros <dgros@earthlink.net>
2014-03-10 23:30:48 -0500

    add mobile menu panel

--- templates/HeadTemplate.php
+++ templates/HeadTemplate.php
@@ -13,9 +13,18 @@ class HeadTemplate extends Template {
     <link rel="stylesheet" type="text/css" href="/SuperCSSLoader.php"></link>
     <link href='http://fonts.googleapis.com/css?family=Alef:400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+    <!--<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />-->
     <script src="/jquery-1.11.0.min.js"></script>
+    <!--<script>
+    $(document).one("mobileinit", function () {
+    $.mobile.ignoreContentEnabled=true;
+    $.mobile.defaultPageTransition = 'none';
+    });
+    </script>
+    <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>-->
     <script src="/icheck.min.js"></script>
     <script src="/alertify.min.js"></script>
+    <script src="/jquery.sidr.min.js"></script>
     <script src="/scripts.php"></script>
   </head>
   <body>
