144ccc92a6a5e0f5435883033d0a0943166ddd57
David Gros <dgros@earthlink.net>
2014-03-11 15:36:25 -0500

    played with viewport some more

--- templates/HeadTemplate.php
+++ templates/HeadTemplate.php
@@ -9,7 +9,7 @@ class HeadTemplate extends Template {
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=0.5">
     <title>624 Scouting</title>
     <link rel="stylesheet" type="text/css" href="/SuperCSSLoader.php"></link>
     <link href='http://fonts.googleapis.com/css?family=Alef:400,700' rel='stylesheet' type='text/css'>
