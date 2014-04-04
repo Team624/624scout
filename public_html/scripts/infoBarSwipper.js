74f4786c016c21c7d35f0fde98eae407f3548298
David Gros <dgros@earthlink.net>
2014-04-03 22:20:30 -0500

    bar swippyness with added stuff

---
+++ public_html/scripts/infoBarSwipper.js
@@ -0,0 +1,22 @@
+//submit.js
+(function() { 
+
+  $(document).ready(function() {
+    /*alert("worky");*/
+   /* var element = document.getElementById('auto-bar');
+    var hammertime = Hammer(element).on("tap", function(event) {
+        alert('hello!');
+    });*/
+    /*var hammer_options = {};
+    $('#auto-bar')
+      .hammer(hammer_options)
+      .on("tap",function(event){
+        alert("hammer dat info");
+      });*/
+  });
+  /*function swipeifyStuff(){
+    alert("STOP! hammer time");
+  }*/
+
+})();
+
